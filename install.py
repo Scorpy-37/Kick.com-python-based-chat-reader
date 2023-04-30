3dace6b28c885884dc6a657562d360dd50ce1adf
Scorpy-37 <122223312+Scorpy-37@users.noreply.github.com>
2023-04-30 17:33:38 +0200

    Update v1.8.1
    v1.8.1
    - fixed install.py not installing ucd properly

--- install.py
+++ install.py
@@ -1,7 +1,7 @@
 from os import system
 
-system("pip install undetected-chromedriver")
+system("pip install undetected_chromedriver")
 system("pip install bs4")
 system("pip install tkinter")
 
-input("Installation done! You can now run main.py")
+input("Installation done! You can now run executor.py")
